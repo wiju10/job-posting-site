973df03ab0e1e7d165fe5525e21de34db10fe9e3
wiju <wiju10@hotmail.com>
2023-01-30 10:26:39 +0800

    tailwind and daisy config setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/about">About my site</a>
+<button class="btn">Button</button>
+<span class="badge">Badge</span>
