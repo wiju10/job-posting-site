0d7c7a84f8212fbcceea5b0a6048d012d570a00c
wiju <wiju10@hotmail.com>
2023-02-01 14:57:07 +0800

    general update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,34 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/about">About my site</a>
-<button class="btn">Button</button>
-<span class="badge">Badge</span>
+<script>
+  import humanize from 'humanize-plus';
+  export let data;
+</script>
+
+<h1 class="text-center text-6xl font-bold">Find Your Next Job</h1>
+
+<div class="overflow-x-auto w-full">
+  {#each data.jobs as job}
+      <div class="flex flex-col mt-10">
+          <div>
+              <a class="font-bold text-2xl" href="/jobs/{job.id}">{job.title}</a>
+              <div class="text-sm mt-1">
+                  {job.employer} . {job.location} .
+                  <span class="text-sm">USD {humanize.formatNumber(job.minAnnualCompensation)} - USD {humanize.formatNumber(
+                          job.maxAnnualCompensation
+                      )}</span>
+              </div>
+              <div class="italic text-xs opacity-50 mt-2">
+                 posted {new Date(job.created).toLocaleDateString(undefined, {
+                      weekday: 'long',
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric'
+                 })}
+            </div>
+          </div>
+
+          <div class="mt-4 ">
+              {job.description.slice(0, 240)}...
+          </div>
+      </div>
+  {/each}
+</div>
